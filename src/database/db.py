5ec40fb483c0e5a8811711933e461346ff31db93
Hannakua <126361524+Hannakua@users.noreply.github.com>
2023-10-06 01:21:21 +0300

    Update db.py

--- src/database/db.py
+++ src/database/db.py
@@ -2,12 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.conf.config import settings
 
-# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:567234@localhost:5432/contactsdb"
-# docker run --name contactsdb -p 5432:5432 -e POSTGRES_PASSWORD=567234 -d postgres
-
 SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
-
-# SQLALCHEMY_DATABASE_URL = "sqlite:///mycontacts.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
